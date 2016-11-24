0e94dd5fe52168d8738cc1ebb8ec4a193d0d94fc
Zulwiyoza Putra <zulwiyozaputra@zulwiyozas-mbp.lan>
2016-11-23 19:03:02 -0800

    Working Version

--- Image Picker Controller Example/ViewController.swift
+++ Image Picker Controller Example/ViewController.swift
@@ -8,18 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imagePickedView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //Tells the delegate that the user picked a still image or movie.
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let image = info[UIImagePickerControllerEditedImage] as? UIImage {
+            self.imagePickedView.image = image
+        }
+        
+        dismiss(animated: true, completion: nil)
+        
     }
-
-
+    
+    //Tells the delegate that the user cancelled the pick operation.
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        
+        dismiss(animated: true, completion: nil)
+        
+    }
+    
+    //Picking an image action
+    @IBAction func pickAnImage(_ sender: Any) {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.allowsEditing = true
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
+        
+    }
 }
 
